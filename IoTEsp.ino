47f387b1c87fadf9c772f3e248676167ea4622b0
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-01-02 16:58:20 -0500

    Programa para el monitoreo de la humedad y temperatura por medio del DHT11

--- IoTEsp.ino
+++ IoTEsp.ino
@@ -5,19 +5,24 @@
   Desarrollado por: César Augusto Álvarez Gaspar
   Fecha: 2 de enero de 2019
 
-  Versión: 0.0.3
-  Descripción: Programa para el uso del puerto serial y graficación de 2 señales
+  Versión: 0.0.4
+  Descripción: Programa para el monitoreo de humedad y temperatura
 */
 
 
 #include <dummy.h> //Libreria para identificar los pines del EPS32
+#include "DHT.h"  //Libreria para el DHT11
 
 
 #define LED_BUILTIN 27 
 #define LED_TEST 0
 #define TAZA_SERIAL 115200  //Velocidad por defecto en el ESP32
-#define TAZA_MUESTREO 1000
+#define TAZA_MUESTREO 5000 //Taza minima para el sensor
 
+#define DTHPIN 26      //Pin al que se conecto el sensor
+#define DTHTYPE DHT11 //Tipo de sensor
+
+DHT dht(DTHPIN, DTHTYPE); //Declaración del Objeto DHT
 
 
 void setup() {
@@ -26,16 +31,27 @@ void setup() {
   pinMode(LED_TEST, OUTPUT);
   //Inicialización de la comunicación serial
   Serial.begin(TAZA_SERIAL);
-  Serial.println("\nPrueba de puerto serial para ESP32");
+  Serial.println("\nPrograma para monitoreo de humedad y temperatura");
+  //Inicialización del sensor
+  dht.begin();
+  Serial.print(0);
+  Serial.print(",");
+  Serial.println(0);
 }
 
-int i=0;
-float j=0;
+
+float h=0;//Humedad
+float t=0;//Temperatura
+
 
 void loop() {
-  Serial.print(i++);
+  //Lectura del sensor
+  h=dht.readHumidity();
+  t=dht.readTemperature();
+  //Visualización de las lecturas
+  Serial.print(h);
   Serial.print(",");
-  j=i+random(-5,5);
-  Serial.println(j++);
+  Serial.println(t);
+  //Muestreo
   delay(TAZA_MUESTREO);  
 }
