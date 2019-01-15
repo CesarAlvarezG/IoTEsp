b717310a4a67f7b41554d43c116420a9c7f509fe
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-01-15 16:16:31 -0500

    Paralelismo de la lectura del sensor DHT11

--- IoTEsp.ino
+++ IoTEsp.ino
@@ -5,23 +5,27 @@
   Desarrollado por: César Augusto Álvarez Gaspar
   Fecha: 15 de enero de 2019
 
-  Versión: 0.0.6
-  Descripción: Programa para el monitoreo de humedad y temperatura, visualizado por medio de un pantalla OLED
-               Prueba de paralelismo con los led LED_TEST y LED_BUILTIN
+  Versión: 0.0.7
+  Descripción: Programa para el monitoreo de humedad y temperatura, visualizado por medio de una pantalla OLED
+               Prueba de paralelismo con los leds LED_TEST y LED_BUILTIN
+               Paralelismo de la lectura del sensor y el refresco de la pantalla
+
+
+               Nota: Encontré dificultades para usar Ticker y el Objeto Figura, por lo cual lo dejo en el loop()
 */
 
 
 #include <dummy.h> //Libreria para identificar los pines del EPS32
-#include "DHT.h"  //Libreria para el DHT11
 #include <Wire.h> //Libreria para el manejo del puerto I2C
+#include <Ticker.h>//Libreria para el manejo de las tareas paralelas
+#include "DHT.h"  //Libreria para el DHT11
 #include "SSD1306Wire.h"//Libreria para el manejo de la pantalla OLED
 #include "IotViewOled.h"//Libreria con interfaz grafica implementada en la pantalla OLED
-#include <Ticker.h>//Libreria para el manejo de las tareas paralelas
 
 #define LED_BUILTIN 27 
 #define LED_TEST 0
 #define TAZA_SERIAL 115200  //Velocidad por defecto en el ESP32
-#define TAZA_MUESTREO 5000 //Taza minima para el sensor
+#define TAZA_REFRESCO_PANTALLA 100//Taza de refresco de la pantalla
 
 
 #define DTHPIN 25      //Pin al que se conecto el sensor
@@ -33,24 +37,35 @@ DHT dht(DTHPIN, DTHTYPE); //Declaración del Objeto DHT
 IotViewPantalla Figura;//Declaración del Objeto Figura
 SSD1306Wire  display(0x3c, 21, 22);//Declaración del Objeto display
 
-Ticker TickerLedBuiltin;//Declaración de la tarea para refrescar el pin LED_BUILTIN 
-Ticker TickerLedTest;//Declaración de la tarea para refrescar el pin LED_TEST 
+Ticker TickerSensorDHT;//Declaración de la tarea de leer el sensor
 
-//Tazas de refresco de las tareas
+//Taza de refresco de la tarea
+float tazaRefrescoSensorDHT =5;//Tiempo en segundos
 
-float tazaRefrescoLedBuiltin =0.1;//Tiempo en segundos
-float tazaRefrescoLedTest =0.5;//Tiempo en segundos
+float h=0;//Humedad
+float t=0;//Temperatura
+int i=0;//Numero de iteracción
 
-//Definición de las tareas de refresco
-void refrescarLedBuiltin() {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+void leerSensorDHT() {
+  
+  //Lectura del sensor
+  h=dht.readHumidity();
+  t=dht.readTemperature();
+  
+  //Visualización de las lecturas
+  Serial.print(h);
+  Serial.print(",");
+  Serial.println(t);
+  
+  //Visualización en la pantalla OLED
+  Figura.SetTrabajoVar1(t);
+  Figura.SetTrabajoVar2(h);
+  Figura.SetApoyoVar2(i++); 
 }
 
-void refrescarLedTest() {
-  digitalWrite(LED_TEST, !digitalRead(LED_TEST));
+void refrescarPantalla() {
+  Figura.Display();  //Función encargada de refrescar la pantalla
 }
-
-
 
 void setup() {
   // Inicialización de los pines.
@@ -70,10 +85,10 @@ void setup() {
   Figura.SetTrabajoEtiqueta2("H [%]"); 
   Figura.SetApoyoEtiqueta1("Ts");
   Figura.SetApoyoEtiqueta2("Iteracción");
-  Figura.SetApoyoVar1(TAZA_MUESTREO);
-  Figura.SetApoyoVar2(3);
-  Figura.SetTrabajoVar1(2);
-  Figura.SetTrabajoVar2(3); 
+  Figura.SetApoyoVar1(tazaRefrescoSensorDHT);
+  Figura.SetApoyoVar2(0);
+  Figura.SetTrabajoVar1(0);
+  Figura.SetTrabajoVar2(0); 
 
   //Inicialización del sensor
   dht.begin();
@@ -83,33 +98,14 @@ void setup() {
   Serial.print(",");
   Serial.println(0);
 
-  //Inicialización del parapadeo de los leds
-  TickerLedBuiltin.attach(tazaRefrescoLedBuiltin, refrescarLedBuiltin);
-  TickerLedTest.attach(tazaRefrescoLedTest, refrescarLedTest);
-  
+  //Inicialización de la tarea de refresco
+  TickerSensorDHT.attach(tazaRefrescoSensorDHT, leerSensorDHT);
 }
 
 
-float h=0;//Humedad
-float t=0;//Temperatura
-int i=0;//Numero de iteracción
 
-void loop() {
-  //Lectura del sensor
-  h=dht.readHumidity();
-  t=dht.readTemperature();
-  
-  //Visualización de las lecturas
-  Serial.print(h);
-  Serial.print(",");
-  Serial.println(t);
-  
-  //Visualización en la pantalla OLED
-  Figura.SetTrabajoVar1(t);
-  Figura.SetTrabajoVar2(h);
-  Figura.SetApoyoVar2(i++);
-  Figura.Display();  
-  
-  //Muestreo
-  delay(TAZA_MUESTREO);  
+void loop() 
+{
+ Figura.Display();
+ delay(TAZA_REFRESCO_PANTALLA);
 }
