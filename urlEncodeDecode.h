a2cd14f2c34305277f7ee3d6c13168325ad315d4
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-05-07 12:37:58 -0500

    Libreria para el manejo de la codificación url

---
+++ urlEncodeDecode.h
@@ -0,0 +1,11 @@
+#ifndef __URLENCODEDECODE_H__
+#define __URLENCODEDECODE_H__
+
+#include <Arduino.h>
+
+String urldecode(String str);
+String urlencode(String str);
+unsigned char h2int(char c);
+
+
+#endif
